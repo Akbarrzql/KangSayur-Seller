a50981e33d08e641a35dfe5bd51ed9fc3bd0609b
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-05-08 10:35:30 +0700

    style [AR]: deleted acces token and style id

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,9 @@
+import 'package:latlong2/latlong.dart';
+
+class AppConstants {
+  static const String mapBoxAccessToken = 'MAPBOX_ACCESS_TOKEN';
+
+  static const String mapBoxStyleId = 'MAPBOX_STYLE_ID';
+
+  static final myLocation = LatLng(-6.807778, 110.841623);
+}
