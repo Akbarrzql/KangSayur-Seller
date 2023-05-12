72f3b1ce64f96409c935fd6a46ff9def4616ca77
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-05-12 17:55:50 +0700

    style [AR]: create ui transaksi

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,9 +1,9 @@
 import 'package:latlong2/latlong.dart';
 
 class AppConstants {
-  static const String mapBoxAccessToken = 'sk.eyJ1IjoiYWtiYXJyenFsMDgiLCJhIjoiY2xoOGtmaGhmMDlqZDNkcHMydDdqc3VpaiJ9.KubCryQvXJv8aOtbq0yhXQ';
+  static const String mapBoxAccessToken = 'MAPBOX_ACCESS_TOKEN';
 
-  static const String mapBoxStyleId = 'https://api.mapbox.com/styles/v1/akbarrzql08/clh8nzyp500wh01pg5x086mq3/tiles/256/{z}/{x}/{y}@2x?access_token=sk.eyJ1IjoiYWtiYXJyenFsMDgiLCJhIjoiY2xoOGtmaGhmMDlqZDNkcHMydDdqc3VpaiJ9.KubCryQvXJv8aOtbq0yhXQ';
+  static const String mapBoxStyleId = 'MAPBOX_STYLE_ID';
 
   static final myLocation = LatLng(-6.807778, 110.841623);
 }
