2f4a3295b51012c94df304a0621c658548c5acb8
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2023-08-15 12:24:58 +0700

    feat [AR]: add deeplink flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:kangsayur_seller/ui/auth/password/ganti_password.dart';
 import 'package:kangsayur_seller/ui/auth/register/driver/reset_pasword_driver.dart';
 import 'package:kangsayur_seller/ui/splash_screen/splash_screen.dart';
 import 'package:kangsayur_seller/ui/transaksi/transaksi.dart';
@@ -20,6 +22,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  final FirebaseDynamicLinks dynamicLinks = FirebaseDynamicLinks.instance;
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await PushNotificationConfig().requestPermission();
   await PushNotificationConfig().androidNotificationChanel();
@@ -31,12 +34,37 @@ void main() async {
   ]);
   // FirebaseNotificationManager.configureForegroundMessaging();
   // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  final PendingDynamicLinkData? initialLink =
+  await dynamicLinks.getInitialLink();
+  _handleDeepLink(initialLink);
 
-  runApp(const MyApp());
+  dynamicLinks.onLink.listen((dynamicLink) {
+    _handleDeepLink(dynamicLink);
+  }, onError: (e) {
+    print('onLinkError');
+    print(e.message);
+  });
+
+  _handleDeepLink(initialLink);
 }
+
+void _handleDeepLink(PendingDynamicLinkData? data) {
+  final Uri? uri = data?.link;
+  print('uri: $uri');
+  if (uri != null && uri.path == '/') {
+    runApp(const MaterialApp(
+      home: GantiPasswordPage(),
+    ));
+  } else {
+    runApp(const MyApp());
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  static final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
   // This widget is the root of your application.
   @override
--- lib/ui/auth/password/ganti_password.dart
+++ lib/ui/auth/password/ganti_password.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:kangsayur_seller/ui/widget/main_button.dart';
 import 'package:kangsayur_seller/ui/widget/textfiled.dart';
 
 import '../../../common/color_value.dart';
+import '../../../validator/input_validator.dart';
+import '../../bottom_navigation/bottom_navigation.dart';
+import '../../widget/custom_textfield.dart';
 
 class GantiPasswordPage extends StatefulWidget {
   const GantiPasswordPage({Key? key}) : super(key: key);
@@ -13,7 +17,6 @@ class GantiPasswordPage extends StatefulWidget {
 
 class _GantiPasswordPageState extends State<GantiPasswordPage> {
 
-  final _passwordLamaController = TextEditingController();
   final _passwordBaruController = TextEditingController();
   final _passwordKonfirmasiBaruController = TextEditingController();
 
@@ -33,7 +36,12 @@ class _GantiPasswordPageState extends State<GantiPasswordPage> {
         ),
         leading: IconButton(
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const BottomNavigation(),
+                )
+            );
           },
           icon: const Icon(
             Icons.arrow_back,
@@ -47,17 +55,6 @@ class _GantiPasswordPageState extends State<GantiPasswordPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Password Lama",
-              style: textTheme.headline6!.copyWith(
-                color: ColorValue.neutralColor,
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(height: 10,),
-            textfield(context, "Masukkan Password Lama", _passwordLamaController, TextInputType.text),
-            const SizedBox(height: 20,),
-            Text(
               "Password Baru",
               style: textTheme.headline6!.copyWith(
                 color: ColorValue.neutralColor,
@@ -66,7 +63,13 @@ class _GantiPasswordPageState extends State<GantiPasswordPage> {
               ),
             ),
             const SizedBox(height: 10,),
-            textfield(context, "Masukkan Password Baru", _passwordBaruController, TextInputType.text),
+            CustomTextFormField(
+              controller: _passwordBaruController,
+              label: 'Masukkan Password Baru Anda',
+              isPassword: true,
+              textInputType: TextInputType.visiblePassword,
+              validator: (value) => InputValidator.passwordValidator(value),
+            ),
             const SizedBox(height: 20,),
             Text(
               "Konfirmasi Password Baru",
@@ -77,8 +80,14 @@ class _GantiPasswordPageState extends State<GantiPasswordPage> {
               ),
             ),
             const SizedBox(height: 10,),
-            textfield(context, "Konfirmasi Password Baru Anda", _passwordKonfirmasiBaruController, TextInputType.text),
-            Spacer(),
+            CustomTextFormField(
+              controller: _passwordKonfirmasiBaruController,
+              label: 'Masukkan Password Baru Anda',
+              isPassword: true,
+              textInputType: TextInputType.visiblePassword,
+              validator: (value) => InputValidator.passwordValidator(value),
+            ),
+            const Spacer(),
             main_button("Simpan", context, onPressed: (){})
           ],
         ),
